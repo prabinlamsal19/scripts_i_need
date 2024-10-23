4c87ed0ad2e230b8998525a2a810be954bcfec32
prabinlamsal19 <lamsalprabin74@gmail.com>
2024-10-23 09:50:13 +0545

    feat: my startup script

---
+++ sage-startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+code /home/user/Documents/edm_flutter &
+
+gnome-terminal &
+
+google-chrome "https://github.com/prabinlamsal19" "https://chatgpt.com" "https://github.com/sageflick/edm_flutter" &
+
+# Wait for all processes to finish starting
+wait
+
